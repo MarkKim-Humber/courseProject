6ad112500e0eeca1924edf74ab2808a49fb252ea
MarkKim-Humber <n01716085@humber.ca>
2024-12-03 22:33:44 -0500

    final css added

--- script.js
+++ script.js
@@ -1,53 +1,15 @@
 // Nav Bar Hover
-const navLi = document.querySelectorAll("#navLi")
+const navLi = document.querySelectorAll("#navLi");
 
 console.log(navLi);
 
 navLi.forEach((listItem) => {
-    listItem.addEventListener("mouseover", () => {
-        listItem.style.backgroundColor = "#ffae00";
-        
-    })
+  listItem.addEventListener("mouseover", () => {
+    listItem.style.backgroundColor = "#ffae00";
+  });
 
-    listItem.addEventListener("mouseout", () => {
-        listItem.style.backgroundColor = "#303741";
-    })
-})
-
-
-
-// Form submission - sending data to https://formspree.io/
-const form = document.getElementById("myForm");
-
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  const status = document.getElementById("myFormStatus");
-  const data = new FormData(event.target);
-  fetch(event.target.action, {
-    method: form.method,
-    body: data,
-    headers: {
-      Accept: "application/json",
-    },
-  })
-    .then((response) => {
-      if (response.ok) {
-        status.innerHTML = "Thank you! I will be in touch soon 😃";
-        form.reset();
-      } else {
-        response.json().then((data) => {
-          if (Object.hasOwn(data, "errors")) {
-            status.innerHTML = data["errors"]
-              .map((error) => error["message"])
-              .join(", ");
-          } else {
-            status.innerHTML = "Oops! There was a problem submitting your form";
-          }
-        });
-      }
-    })
-    .catch(() => {
-      status.innerHTML = "Oops! There was a problem submitting your form";
-    });
+  listItem.addEventListener("mouseout", () => {
+    listItem.style.backgroundColor = "#303741";
+  });
 });
+
