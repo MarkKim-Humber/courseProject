96b7220e66b742caef8d83c5a4ebf3a08a5138d6
MarkKim-Humber <n01716085@humber.ca>
2024-12-05 23:22:45 -0500

    final changes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Nav Bar Hover
 const navLi = document.querySelectorAll("#navLi");
 
+// Using forEach() method to iterate over the array of "navLi" and add Event Listeners to each item
 navLi.forEach((listItem) => {
   listItem.addEventListener("mouseover", () => {
     listItem.style.backgroundColor = "#ffae00";
